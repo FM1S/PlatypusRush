f451beb329436f11025d22cb8ba05549d3a97cf9
FM1S <38359240+FM1S@users.noreply.github.com>
2018-04-14 13:46:56 +0300

    master
    Something

--- ScreenPager/app/src/main/java/com/example/anane/screenpager/ViewPagerSampleActivity.java
+++ ScreenPager/app/src/main/java/com/example/anane/screenpager/ViewPagerSampleActivity.java
@@ -1,22 +1,36 @@
 package com.example.anane.screenpager;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-public class ViewPagerSampleActivity extends Activity {
+public class ViewPagerSampleActivity extends AppCompatActivity {
     /** Called when the activity is first created. */
+    private List<View> allEds;
+    FloatingActionButton fab;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         LayoutInflater inflater = LayoutInflater.from(this);
         List<View> pages = new ArrayList<View>();
+
+//инициализировали наш массив с edittext.aьи
+        allEds = new ArrayList<View>();
+///
+
+///
+
 
         View page = inflater.inflate(R.layout.page, null);
         TextView textView = (TextView) page.findViewById(R.id.text_view);
@@ -48,16 +62,57 @@ public class ViewPagerSampleActivity extends Activity {
         textView.setText("Суббота");
         pages.add(page);
 
-        page = inflater.inflate(R.layout.page, null);
-        textView = (TextView) page.findViewById(R.id.text_view);
-        textView.setText("Воскресенье");
-        pages.add(page);
+
 
         SamplePagerAdapter pagerAdapter = new SamplePagerAdapter(pages);
         ViewPager viewPager = new ViewPager(this);
         viewPager.setAdapter(pagerAdapter);
         viewPager.setCurrentItem(0);
+        /*fab = new FloatingActionButton(this);
 
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final View view = getLayoutInflater().inflate(R.layout.dinamik_day, null);
+                allEds.add(view);
+                final LinearLayout linear = findViewById(R.id.linear);
+                linear.addView(view);
+
+            }
+        });
+        viewPager.addView(fab);
+        */
         setContentView(viewPager);
+
+        FloatingActionButton act1 = (FloatingActionButton) pages.get(0).findViewById(R.id.add_task);
+        act1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final View view = getLayoutInflater().inflate(R.layout.dinamik_day, null);
+                allEds.add(view);
+                final LinearLayout linear = findViewById(R.id.linear);
+                linear.addView(view);
+
+            }
+        });
+
+
     }
+   /* public void onClick(View v) {
+        final View view = getLayoutInflater().inflate(R.layout.dinamik_day, null);
+        allEds.add(view);
+        final LinearLayout linear = (LinearLayout) findViewById(R.id.linear);
+        linear.addView(view);
+    }*/
 }
+/*               FloatingActionButton act1 = (FloatingActionButton) pages.get(0).findViewById(R.id.add_task);
+        act1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final View view = getLayoutInflater().inflate(R.layout.dinamik_day, null);
+                allEds.add(view);
+                final LinearLayout linear = findViewById(R.id.linear);
+                linear.addView(view);
+
+            }
+        });*/
